1ba6f7163b8034d3ff6ccdc78316cadea8f222e0
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-23 14:03:54 +0200

    Added util class for text validators

---
+++ lib/util/validators.dart
@@ -0,0 +1,35 @@
+enum EmailValidationError { emptyInput, invalidEmail }
+enum PasswordValidationError { emptyInput, invalidPassword, weakPassword }
+
+class Validators {
+  static EmailValidationError? email(String? string) {
+    if (string == null || string.isEmpty) {
+      return EmailValidationError.emptyInput;
+    }
+
+    final regExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!regExp.hasMatch(string)) {
+      return EmailValidationError.invalidEmail;
+    }
+
+    return null;
+  }
+
+  static PasswordValidationError? password(String? string) {
+    if (string == null || string.isEmpty) {
+      return PasswordValidationError.emptyInput;
+    }
+
+    final invalidRegExp = RegExp(r'[ ]');
+    if (invalidRegExp.hasMatch(string)) {
+      return PasswordValidationError.invalidPassword;
+    }
+
+    final weakRegExp = RegExp(r'^.{8,}$');
+    if (!weakRegExp.hasMatch(string)) {
+      return PasswordValidationError.weakPassword;
+    }
+
+    return null;
+  }
+}
