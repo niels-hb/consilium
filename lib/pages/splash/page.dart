3032bc01641e010b40cdb3654c1eaacc5201e34a
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-23 15:53:53 +0200

    Added fowarding logic to SplashPage

--- lib/pages/splash/page.dart
+++ lib/pages/splash/page.dart
@@ -1,9 +1,50 @@
+import 'package:consilium/pages/home/page.dart';
+import 'package:consilium/pages/login/page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+enum _LoginState { notLoggedIn, loggedIn }
+
+class SplashPage extends StatefulWidget {
   static const String route = '/splash';
 
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    _bootstrap();
+  }
+
+  Future<void> _bootstrap() async {
+    await Firebase.initializeApp();
+
+    await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+
+    switch (_checkLoginState()) {
+      case _LoginState.notLoggedIn:
+        Navigator.of(context).pushReplacementNamed(LoginPage.route);
+        break;
+      case _LoginState.loggedIn:
+        Navigator.of(context).pushReplacementNamed(HomePage.route);
+        break;
+    }
+  }
+
+  _LoginState _checkLoginState() {
+    if (FirebaseAuth.instance.currentUser == null) {
+      return _LoginState.notLoggedIn;
+    } else {
+      return _LoginState.loggedIn;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
