586e064df609187842992cffaccfd893981cfe9f
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-23 13:50:46 +0200

    Added initial splash, home and login pages

---
+++ lib/pages/home/page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HomePage extends StatelessWidget {
+  static const String route = '/';
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appName),
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash/page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  static const String route = '/splash';
+
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
