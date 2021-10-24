1e0959535eea59ed7f8747e28d5175f8a4be5182
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-24 16:43:59 +0200

    Added BottomNavigationBar to HomePage

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,12 +1,24 @@
+import 'package:consilium/pages/home/tab_definition.dart';
+import 'package:consilium/pages/home/tabs/details.dart';
+import 'package:consilium/pages/home/tabs/overview.dart';
+import 'package:consilium/pages/home/tabs/schedule.dart';
 import 'package:consilium/pages/login/page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static const String route = '/home';
 
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentTab = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +36,7 @@ class HomePage extends StatelessWidget {
           }
 
           if (snapshot.hasData) {
-            return Center(
-              child: Text(
-                'Welcome ${snapshot.data!.email}!',
-              ),
-            );
+            return _getTabs()[_currentTab].content;
           }
 
           return Center(
@@ -41,6 +49,41 @@ class HomePage extends StatelessWidget {
           );
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentTab,
+        items: _getTabs().map((tab) => tab.bottomNavigationBarItem).toList(),
+        onTap: (index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+      ),
     );
+  }
+
+  List<TabDefinition> _getTabs() {
+    return [
+      TabDefinition(
+        content: const OverviewTab(),
+        bottomNavigationBarItem: BottomNavigationBarItem(
+          icon: const Icon(Icons.dashboard),
+          label: AppLocalizations.of(context)!.overview,
+        ),
+      ),
+      TabDefinition(
+        content: const ScheduleTab(),
+        bottomNavigationBarItem: BottomNavigationBarItem(
+          icon: const Icon(Icons.schedule),
+          label: AppLocalizations.of(context)!.schedule,
+        ),
+      ),
+      TabDefinition(
+        content: const DetailsTab(),
+        bottomNavigationBarItem: BottomNavigationBarItem(
+          icon: const Icon(FontAwesomeIcons.chartArea),
+          label: AppLocalizations.of(context)!.details,
+        ),
+      ),
+    ];
   }
 }
---
+++ lib/pages/home/tabs/details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DetailsTab extends StatelessWidget {
+  const DetailsTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('DetailsTab'),
+    );
+  }
+}
