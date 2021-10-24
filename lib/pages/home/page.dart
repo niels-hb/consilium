5b8e50179724d7b75f5fa06127a51c7de2af101a
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-24 10:25:28 +0200

    Added placeholder content to HomePage

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,3 +1,5 @@
+import 'package:consilium/pages/login/page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -11,6 +13,33 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.appName),
+      ),
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return Center(
+              child: Text(
+                'Welcome ${snapshot.data!.email}!',
+              ),
+            );
+          }
+
+          return Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(LoginPage.route);
+              },
+              child: Text(AppLocalizations.of(context)!.signIn),
+            ),
+          );
+        },
       ),
     );
   }
