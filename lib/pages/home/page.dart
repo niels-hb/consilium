8aaddee11cc4882606b8bfd83c26d5d033a01b23
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-29 15:07:37 +0200

    Restructured HomePage

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -8,17 +8,93 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   static const String route = '/home';
 
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(AppLocalizations.of(context)!.appName),
+            ),
+            body: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        if (snapshot.hasData) {
+          return const _LoggedInPage();
+        }
+
+        return const _LoggedOutPage();
+      },
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoggedOutPage extends StatelessWidget {
+  const _LoggedOutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)!.appName),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => _signIn(context),
+          child: Text(AppLocalizations.of(context)!.signIn.toUpperCase()),
+        ),
+      ),
+    );
+  }
+
+  void _signIn(BuildContext context) {
+    Navigator.of(context).pushNamed(LoginPage.route);
+  }
+}
+
+class _LoggedInPage extends StatefulWidget {
+  const _LoggedInPage({Key? key}) : super(key: key);
+
+  @override
+  State<_LoggedInPage> createState() => _LoggedInPageState();
+}
+
+class _LoggedInPageState extends State<_LoggedInPage> {
   int _currentTab = 0;
+
+  List<TabDefinition> get _tabs => [
+        TabDefinition(
+          content: const OverviewTab(),
+          bottomNavigationBarItem: BottomNavigationBarItem(
+            icon: const Icon(Icons.dashboard),
+            label: AppLocalizations.of(context)!.overview,
+          ),
+        ),
+        TabDefinition(
+          content: const ScheduleTab(),
+          bottomNavigationBarItem: BottomNavigationBarItem(
+            icon: const Icon(Icons.schedule),
+            label: AppLocalizations.of(context)!.schedule,
+          ),
+        ),
+        TabDefinition(
+          content: const DetailsTab(),
+          bottomNavigationBarItem: BottomNavigationBarItem(
+            icon: const Icon(FontAwesomeIcons.chartArea),
+            label: AppLocalizations.of(context)!.details,
+          ),
+        ),
+      ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,32 +108,10 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          if (snapshot.hasData) {
-            return _getTabs()[_currentTab].content;
-          }
-
-          return Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(LoginPage.route);
-              },
-              child: Text(AppLocalizations.of(context)!.signIn.toUpperCase()),
-            ),
-          );
-        },
-      ),
+      body: _tabs[_currentTab].content,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentTab,
-        items: _getTabs().map((tab) => tab.bottomNavigationBarItem).toList(),
+        items: _tabs.map((tab) => tab.bottomNavigationBarItem).toList(),
         onTap: (index) {
           setState(() {
             _currentTab = index;
@@ -68,33 +122,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   void _signOut() async {
-    await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacementNamed(LoginPage.route);
-  }
-
-  List<TabDefinition> _getTabs() {
-    return [
-      TabDefinition(
-        content: const OverviewTab(),
-        bottomNavigationBarItem: BottomNavigationBarItem(
-          icon: const Icon(Icons.dashboard),
-          label: AppLocalizations.of(context)!.overview,
-        ),
-      ),
-      TabDefinition(
-        content: const ScheduleTab(),
-        bottomNavigationBarItem: BottomNavigationBarItem(
-          icon: const Icon(Icons.schedule),
-          label: AppLocalizations.of(context)!.schedule,
-        ),
-      ),
-      TabDefinition(
-        content: const DetailsTab(),
-        bottomNavigationBarItem: BottomNavigationBarItem(
-          icon: const Icon(FontAwesomeIcons.chartArea),
-          label: AppLocalizations.of(context)!.details,
-        ),
-      ),
-    ];
+    FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushNamed(LoginPage.route);
   }
 }
