03c8143b270d448966820d58d564f650cdafb993
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-29 19:29:59 +0200

    Added breakpoints for maximum width

--- lib/util/custom_theme.dart
+++ lib/util/custom_theme.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/cupertino.dart';
 
 class CustomTheme {
+  static const double _breakpointSmall = 600;
+  static const double _breakpointMedium = 1200;
+
   static double getMaxWidth(BuildContext context) =>
-      0.8 * MediaQuery.of(context).size.width;
+      _getWidthMultiplier(context) * MediaQuery.of(context).size.width;
+
+  static double _getWidthMultiplier(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    if (width <= _breakpointSmall) {
+      return 0.9;
+    } else if (width <= _breakpointMedium) {
+      return 0.8;
+    } else {
+      return 0.5;
+    }
+  }
 }
