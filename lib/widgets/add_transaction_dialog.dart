b7ca6413a967dfc8990a153c54c260fcf9641b2f
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-31 17:18:52 +0100

    Added initial AddTransactionDialog

--- lib/widgets/add_transaction_dialog.dart
+++ lib/widgets/add_transaction_dialog.dart
@@ -1,5 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:consilium/models/category.dart';
+import 'package:consilium/models/transaction_model.dart';
+import 'package:consilium/services/firebase_service.dart';
+import 'package:consilium/util/custom_theme.dart';
+import 'package:consilium/util/validators.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class AddTransactionDialog extends StatefulWidget {
   const AddTransactionDialog({Key? key}) : super(key: key);
@@ -10,6 +19,15 @@ class AddTransactionDialog extends StatefulWidget {
 
 class AddTransactionDialogState extends State<AddTransactionDialog> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _amountController = TextEditingController();
+  Category? _category;
+  final TextEditingController _categoryController = TextEditingController();
+  DateTime _createdOn = DateTime.now();
+  final TextEditingController _createdOnController = TextEditingController(
+    text: CustomTheme.getDefaultDateFormat().format(DateTime.now()),
+  );
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
 
   bool _active = true;
 
@@ -21,8 +39,49 @@ class AddTransactionDialogState extends State<AddTransactionDialog> {
         key: _formKey,
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: const [
-            Text('TODO'),
+          children: [
+            TextFormField(
+              controller: _createdOnController,
+              decoration: CustomTheme.getDefaultInputDecoration(
+                labelText: AppLocalizations.of(context)!.createdOn,
+              ),
+              onTap: _showDatePicker,
+              readOnly: true,
+            ),
+            const SizedBox(height: 8.0),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _nameController,
+              decoration: CustomTheme.getDefaultInputDecoration(
+                labelText: AppLocalizations.of(context)!.name,
+              ),
+              keyboardType: TextInputType.text,
+              validator: _validateName,
+            ),
+            const SizedBox(height: 8.0),
+            _buildCategoryFormField(),
+            const SizedBox(height: 8.0),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              controller: _amountController,
+              decoration: CustomTheme.getDefaultInputDecoration(
+                labelText: AppLocalizations.of(context)!.amount,
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                signed: false,
+                decimal: true,
+              ),
+              validator: _validateName,
+            ),
+            const SizedBox(height: 8.0),
+            TextFormField(
+              controller: _noteController,
+              decoration: CustomTheme.getDefaultInputDecoration(
+                labelText: AppLocalizations.of(context)!.note,
+              ),
+              keyboardType: TextInputType.text,
+              maxLines: 4,
+            ),
           ],
         ),
       ),
@@ -39,6 +98,47 @@ class AddTransactionDialogState extends State<AddTransactionDialog> {
     );
   }
 
+  TypeAheadFormField _buildCategoryFormField() {
+    return TypeAheadFormField<Category>(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      itemBuilder: (BuildContext context, Category suggestion) => ListTile(
+        title: Text(suggestion.translation(context)),
+      ),
+      onSuggestionSelected: (Category suggestion) {
+        _category = suggestion;
+        _categoryController.text = suggestion.translation(context);
+      },
+      suggestionsCallback: (pattern) => Category.values.where(
+        (category) => category
+            .translation(context)
+            .toLowerCase()
+            .contains(pattern.trim().toLowerCase()),
+      ),
+      textFieldConfiguration: TextFieldConfiguration(
+        controller: _categoryController,
+        decoration: CustomTheme.getDefaultInputDecoration(
+          labelText: AppLocalizations.of(context)!.category,
+        ),
+        keyboardType: TextInputType.text,
+      ),
+      validator: _validateCategory,
+    );
+  }
+
+  void _showDatePicker() async {
+    DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: _createdOn,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2900),
+    );
+
+    _createdOn = date ?? DateTime.now();
+    _createdOnController.text = CustomTheme.getDefaultDateFormat().format(
+      _createdOn,
+    );
+  }
+
   void _cancel() {
     Navigator.of(context).pop();
   }
@@ -52,10 +152,51 @@ class AddTransactionDialogState extends State<AddTransactionDialog> {
       _active = false;
     });
 
-    await Future.delayed(const Duration(seconds: 1));
+    try {
+      await FirebaseService.getTransactionsCollection().add(
+        TransactionModel(
+          amountCents: (double.parse(_amountController.text) * 100).round(),
+          category: _category ?? Category.miscellaneous,
+          createdOn: _createdOn,
+          name: _nameController.text,
+          uid: FirebaseAuth.instance.currentUser!.uid,
+          note: _noteController.text.isEmpty ? null : _noteController.text,
+        ),
+      );
+    } on FirebaseException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.message.toString()),
+        ),
+      );
+    }
 
     setState(() {
       _active = true;
     });
+
+    Navigator.of(context).pop();
+  }
+
+  String? _validateName(String? name) {
+    switch (Validators.required(name)) {
+      case ValidationError.emptyInput:
+        return AppLocalizations.of(context)!.emptyInput;
+      default:
+        break;
+    }
+  }
+
+  String? _validateCategory(String? category) {
+    switch (Validators.required(category)) {
+      case ValidationError.emptyInput:
+        return AppLocalizations.of(context)!.emptyInput;
+      default:
+        break;
+    }
+
+    if (CategoryHelper.fromTranslation(context, category) == null) {
+      return AppLocalizations.of(context)!.invalidCategory;
+    }
   }
 }
