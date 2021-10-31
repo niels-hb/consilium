92a986cb35b6a46685b3b7469725128a0b2e3aa7
Niels Heidbrink <niels.heidbrink@gmail.com>
2021-10-31 19:52:10 +0100

    Removed GestureDetector from ListTiles

--- lib/widgets/schedule_list_tile.dart
+++ lib/widgets/schedule_list_tile.dart
@@ -15,48 +15,37 @@ class ScheduleListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _onClick(context),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Icon(schedule.data().category.icon()),
-                const SizedBox(width: 16.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(schedule.data().name),
-                    Text(
-                      CustomTheme.getDefaultDateFormat().format(
-                        schedule.data().nextPaymentOn,
-                      ),
-                      style: Theme.of(context).textTheme.caption,
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(schedule.data().category.icon()),
+              const SizedBox(width: 16.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(schedule.data().name),
+                  Text(
+                    CustomTheme.getDefaultDateFormat().format(
+                      schedule.data().nextPaymentOn,
                     ),
-                  ],
-                ),
-              ],
-            ),
-            Text(
-              NumberFormat.currency(
-                locale: 'de',
-                symbol: '€',
-              ).format(schedule.data().signedAmountCents / 100),
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _onClick(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('TODO'),
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(
+            NumberFormat.currency(
+              locale: 'de',
+              symbol: '€',
+            ).format(schedule.data().signedAmountCents / 100),
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/transaction_list_tile.dart
+++ lib/widgets/transaction_list_tile.dart
@@ -15,48 +15,37 @@ class TransactionListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => _onClick(context),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Icon(transaction.data().category.icon()),
-                const SizedBox(width: 16.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(transaction.data().name),
-                    Text(
-                      CustomTheme.getDefaultDateFormat().format(
-                        transaction.data().createdOn,
-                      ),
-                      style: Theme.of(context).textTheme.caption,
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(transaction.data().category.icon()),
+              const SizedBox(width: 16.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(transaction.data().name),
+                  Text(
+                    CustomTheme.getDefaultDateFormat().format(
+                      transaction.data().createdOn,
                     ),
-                  ],
-                ),
-              ],
-            ),
-            Text(
-              NumberFormat.currency(
-                locale: 'de',
-                symbol: '€',
-              ).format(transaction.data().amountCents / 100),
-              style: Theme.of(context).textTheme.bodyText1,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _onClick(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('TODO'),
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(
+            NumberFormat.currency(
+              locale: 'de',
+              symbol: '€',
+            ).format(transaction.data().amountCents / 100),
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+        ],
       ),
     );
   }
